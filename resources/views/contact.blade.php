8d8e46963c94c3d617b92b5a6cb88a1b76932021
Eduardo Martinez <eduardo.martinez.sanchez@hotmail.com>
2020-06-26 19:17:56 -0500

    Agregando notificacion por email

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,26 @@
-@extends ('layout')
-
-@section ('content')
-    <h1>Hello world</h1>
-@endsection
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body class="bg-gray-100 flex items-center justify-center h-full">
+    <form method="POST" action="/contact" class="bg-white p-6 rounded shadow-md" style="widht:300px">
+        @csrf
+        <div class="mb-5">
+            <label for="email" class="block text-xs uppercase font-semibold mb-1">
+                Email Address
+            </label>
+            <input type="text" name="email" id="email" class="border px-2 py-1 text-sm w-full">
+        </div>
+        <button type="submit" class="bg-blue500 py-2 text-white rounded-full text-sm w-full">
+            Email me
+        </button>
+        @if (session('message'))
+            <p>{{ session('message')}}</p>
+        @endif
+    </form>
+</body>
+</html>
---
+++ resources/views/emails/contact-me.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+# A heading
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime fuga libero ratione quas, obcaecati omnis enim. Nesciunt dolorum ad mollitia inventore iure aspernatur beatae hic in, illum magni ratione molestiae.
+
+- A list 
+- goes 
+- here
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis architecto iste culpa rem nobis, unde dolores cumque explicabo nisi corporis vel in quod fuga soluta, ab quae pariatur magni quam?
+
+@component('mail::button', ['url' => 'https://google.com'])
+Ir a google
+@endcomponent
+@endcomponent
